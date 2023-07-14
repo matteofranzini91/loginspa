9dcd8baeebfbc80463751d60172a0055851fc8e5
Matteo Franzini <frnmtt91@hotmail.it>
2023-07-14 21:30:10 +0200

    Added eslint, prettier, typescript, sass and some some webpack plugins and loaders

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import './assets/layout.scss';
+
+const App = () => <div>Hola</div>;
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,64 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 const path = require('path');
 
 module.exports = (env, argv) => {
-    return {
-        output: {
-            filename: argv.mode == 'production' ? '[name].[contenthash].js' : 'main.js',
-            path: path.resolve(__dirname, 'build')
-        },
-        module: {
-            rules: [
+  return {
+    output: {
+      filename: argv.mode == 'production' ? '[name].[contenthash].js' : 'main.js',
+      path: path.resolve(__dirname, 'build')
+    },
+    resolve: {
+      modules: [__dirname, 'src', 'node_modules'],
+      extensions: ['*', '.js', '.jsx', '.tsx', '.ts']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|ts)x?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-react',
                 {
-                    test: /\.(js)x?$/,
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            [
-                                '@babel/preset-react',
-                                {
-                                    runtime: 'automatic'
-                                }
-                            ],
-                            '@babel/preset-env'
-                        ]
-                    }
+                  runtime: 'automatic'
                 }
+              ],
+              '@babel/preset-env',
+              '@babel/preset-typescript'
             ]
+          }
         },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: 'src/index.html'
-            })
-        ],
-        devServer: {
-            port: 3000,
+        {
+          test: /\.(c|sc|sa)ss$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader
+            },
+            'css-loader',
+            'sass-loader'
+          ]
         },
-        devtool: 'source-map'
-    }
-}
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          exclude: /node_modules/,
+          use: ['file-loader']
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html'
+      }),
+      new ESLintPlugin({
+        extensions: ['js', 'jsx', 'ts', 'tsx']
+      }),
+      new MiniCssExtractPlugin()
+    ],
+    devtool: 'source-map'
+  };
+};
