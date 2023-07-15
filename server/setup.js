5912bf5dfadc06a0eea3d0d204524e5df7623a3d
matteofranzini91 <50463022+matteofranzini91@users.noreply.github.com>
2023-07-15 15:40:01 +0200

    Develop
    * Continued creation of initial project config
    
    * Removed file
    
    * Added eslint, prettier, typescript, sass and some some webpack plugins and loaders
    
    * Installed redux, redux toolkit, axios and react router and complete devServer middleware for mock dev environment responses

---
+++ server/setup.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const bodyParser = require('body-parser');
+const users = require('./mocks/users.json');
+
+const devMiddlewares = (middlewares, devServer, argv) => {
+  if (!devServer) {
+    throw new Error('webpack-dev-server is not defined');
+  }
+
+  if (argv.mode == 'production') return;
+
+  devServer.app.use(bodyParser.json());
+
+  devServer.app.post('/login', bodyParser.json(), function (req, res) {
+    const user = users.find((user) => user.email === req.body.email);
+
+    if (user) res.send(user);
+    else
+      res.status(400).send({
+        message: 'This is an error!'
+      });
+  });
+
+  return middlewares;
+};
+
+module.exports = { devMiddlewares };
